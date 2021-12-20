e17659a78c347e734b0e4046cf48c7504c3fccab
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-21 01:07:37 +0800

    4-测试helloworld

---
+++ src/main/java/top/nzhz/wiki/controller/TestController.java
@@ -0,0 +1,13 @@
+package top.nzhz.wiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
