f75cb0550a27a818e86560dce1b2dfc04290a545
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-21 01:10:11 +0800

    4-测试helloworld

--- src/main/java/top/nzhz/wiki/controller/TestController.java
+++ src/main/java/top/nzhz/wiki/controller/TestController.java
@@ -1,12 +1,14 @@
 package top.nzhz.wiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+//    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World";
     }
