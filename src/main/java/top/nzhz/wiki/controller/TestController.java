9af7e78e2b9b4eb878b8d5d2addf62bb159b490e
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-21 20:17:39 +0800

    5-http测试功能，测试Post

--- src/main/java/top/nzhz/wiki/controller/TestController.java
+++ src/main/java/top/nzhz/wiki/controller/TestController.java
@@ -1,15 +1,27 @@
 package top.nzhz.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
 
+    //    @RequestMapping
+//    @RostMapping
+//    @DeleteMapping
 //    @RequestMapping("/hello")
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World";
+
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello,Post,"+name;
     }
 }
