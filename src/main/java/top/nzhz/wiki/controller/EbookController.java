aeecfbc0d7f1ce128eddcaf4d684f2241860e7fb
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-22 23:53:23 +0800

    17-将返回类EbookResp和原类Ebook区分开,比如密码什么的就不能返回到前端

--- src/main/java/top/nzhz/wiki/controller/EbookController.java
+++ src/main/java/top/nzhz/wiki/controller/EbookController.java
@@ -3,9 +3,9 @@ package top.nzhz.wiki.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import top.nzhz.wiki.domain.Ebook;
 import top.nzhz.wiki.req.EbookReq;
 import top.nzhz.wiki.resp.CommonResp;
+import top.nzhz.wiki.resp.EbookResp;
 import top.nzhz.wiki.service.EbookService;
 
 import javax.annotation.Resource;
@@ -20,8 +20,8 @@ public class EbookController {
 
     @GetMapping("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list=ebookService.list(req);
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list=ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/top/nzhz/wiki/service/EbookService.java
+++ src/main/java/top/nzhz/wiki/service/EbookService.java
@@ -1,13 +1,16 @@
 package top.nzhz.wiki.service;
 
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import top.nzhz.wiki.domain.Ebook;
 import top.nzhz.wiki.domain.EbookExample;
 import top.nzhz.wiki.mapper.EbookMapper;
 import top.nzhz.wiki.req.EbookReq;
+import top.nzhz.wiki.resp.EbookResp;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,11 +20,20 @@ public class EbookService {
 //    @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(EbookReq req){
+    public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+req.getName()+"%");
-        return ebookMapper.selectByExample(ebookExample);
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
 
