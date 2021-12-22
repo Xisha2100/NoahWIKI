c723e406e3652104360f255008c00f865eead957
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-22 23:43:29 +0800

    16-封装请求成一个类

--- src/main/java/top/nzhz/wiki/controller/EbookController.java
+++ src/main/java/top/nzhz/wiki/controller/EbookController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.nzhz.wiki.domain.Ebook;
+import top.nzhz.wiki.req.EbookReq;
 import top.nzhz.wiki.resp.CommonResp;
 import top.nzhz.wiki.service.EbookService;
 
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(String name) {
+    public CommonResp list(EbookReq req) {
         CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list=ebookService.list(name);
+        List<Ebook> list=ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/top/nzhz/wiki/service/EbookService.java
+++ src/main/java/top/nzhz/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import top.nzhz.wiki.domain.Ebook;
 import top.nzhz.wiki.domain.EbookExample;
 import top.nzhz.wiki.mapper.EbookMapper;
+import top.nzhz.wiki.req.EbookReq;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,10 +17,10 @@ public class EbookService {
 //    @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(String name){
+    public List<Ebook> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+name+"%");
+        criteria.andNameLike("%"+req.getName()+"%");
         return ebookMapper.selectByExample(ebookExample);
     }
 }
