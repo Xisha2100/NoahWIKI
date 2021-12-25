92a0a1a8a9d1602984ca324a342eca40c8f6e196
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-25 16:07:29 +0800

    30-Ebook支持动态sql

--- src/main/java/top/nzhz/wiki/service/EbookService.java
+++ src/main/java/top/nzhz/wiki/service/EbookService.java
@@ -3,6 +3,7 @@ package top.nzhz.wiki.service;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 import top.nzhz.wiki.domain.Ebook;
 import top.nzhz.wiki.domain.EbookExample;
 import top.nzhz.wiki.mapper.EbookMapper;
@@ -20,17 +21,19 @@ public class EbookService {
 //    @Autowired
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req){
+    public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> respList = new ArrayList<>();
 
         for (Ebook ebook : ebookList) {
             EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
+            BeanUtils.copyProperties(ebook, ebookResp);
             respList.add(ebookResp);
         }
         return respList;
