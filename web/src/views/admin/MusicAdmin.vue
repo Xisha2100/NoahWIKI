dc6d33679565910f5094fb49b20396a6ea7ab195
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-01-09 00:36:33 +0800

    60-添加查询功能

--- web/src/views/admin/MusicAdmin.vue
+++ web/src/views/admin/MusicAdmin.vue
@@ -6,9 +6,22 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <p>
-        <a-button type="primary" @click="add()" size="large">
-          新增
-        </a-button>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page: 1,size: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="add()" >
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
       </p>
       <a-table
           :columns="columns"
@@ -94,6 +107,8 @@ export default defineComponent({
   name: 'AdminMusic',
   setup() {
     const music = ref();
+    const param =ref();
+    param.value={};
     // const ebooks1 = reactive({books: []});
     const pagination = ref({
       current: 1,
@@ -149,7 +164,8 @@ export default defineComponent({
       axios.get("music/list", {
         params: {
           page: params.page,
-          size: params.size
+          size: params.size,
+          name: param.value.name
         }
       }).then((response) => {
         loading.value = false;
@@ -229,11 +245,13 @@ export default defineComponent({
     });
 
     return {
+      param,
       music,
       pagination,
       columns,
       loading,
       handleTableChange,
+      handleQuery,
 
       edit,
       add,
