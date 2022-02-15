dc28244337813ae5f81b609d74fd4089fedac82f
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-02-15 17:16:29 +0800

    111 - 前端 header添加登录按键

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,12 +1,17 @@
 <template>
+
   <a-layout-header class="header">
-    <div class="logo"/>
+    <a class="login-menu" @click="showLoginModal">
+      <span>登录</span>
+    </a>
+
+
     <a-menu
-        theme="dark"
-        mode="horizontal"
-        :style="{ lineHeight: '64px' }"
+    theme="dark"
+    mode="horizontal"
+    :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="/home">
+      <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
       <a-menu-item key="/admin/user">
@@ -21,20 +26,67 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+
+
+
     </a-menu>
+
+    <a-modal
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName" />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" type="password" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
+
   </a-layout-header>
 </template>
 
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent,ref} from 'vue';
 
 export default defineComponent({
   name: "the-header",
 
+  setup(){
+    const loginUser = ref({
+      loginName:"test",
+      password:"test"
+    });
+
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    };
+
+    const login = () => {
+      console.log("开始登录");
+    };
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login,
+    }
+  }
 });
 </script>
 
-<style scoped>
-
+<style>
+.login-menu {
+  float: right;
+  color: white;
+}
 </style>
