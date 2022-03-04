8d275e37ec78b3018a63c4e3e05c45c2e4bc43fd
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-03-05 00:24:29 +0800

    134- 后端 增加定时任务日志

--- src/main/java/top/nzhz/wiki/job/DocJob.java
+++ src/main/java/top/nzhz/wiki/job/DocJob.java
@@ -11,17 +11,21 @@ import javax.annotation.Resource;
 @Component
 public class DocJob {
 
-   private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
-   @Resource
-   private DocService docService;
-   /**
-    * 自定义cron表达式跑批
-    * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
-    */
-   @Scheduled(cron = "0/30 * * * * ?")
-   public void cron() {
+    @Resource
+    private DocService docService;
+
+    /**
+     * 自定义cron表达式跑批
+     * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
+     */
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void cron() {
+        LOG.info("更新每条音乐下的文档数据");
+        long start=System.currentTimeMillis();
         docService.updateMusicInfo();
-   }
+        LOG.info("更新文档数据，耗时：{}毫秒",System.currentTimeMillis()-start);
+    }
 
 }
