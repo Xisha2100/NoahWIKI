a9b4ad5853462e2fc78986b65343c1998f451121
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-03-06 00:10:12 +0800

    137- 后端 添加日志流水号 sl4fj + logback

--- src/main/java/top/nzhz/wiki/job/DocJob.java
+++ src/main/java/top/nzhz/wiki/job/DocJob.java
@@ -2,9 +2,11 @@ package top.nzhz.wiki.job;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import top.nzhz.wiki.service.DocService;
+import top.nzhz.wiki.utils.SnowFlake;
 
 import javax.annotation.Resource;
 
@@ -16,12 +18,16 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 自定义cron表达式跑批
      * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
      */
     @Scheduled(cron = "0/30 * * * * ?")
     public void cron() {
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新每条音乐下的文档数据");
         long start=System.currentTimeMillis();
         docService.updateMusicInfo();
