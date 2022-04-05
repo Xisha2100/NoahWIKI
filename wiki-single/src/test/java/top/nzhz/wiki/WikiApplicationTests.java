2894127a299ca71809ef7784a7185169320b8e24
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-04-05 23:19:43 +0800

    153- 单元测试重构 现在需要在@SpingBootTest()中指明对应测试类

--- wiki-single/src/test/java/top/nzhz/wiki/WikiApplicationTests.java
+++ wiki-single/src/test/java/top/nzhz/wiki/WikiApplicationTests.java
@@ -3,11 +3,12 @@ package top.nzhz.wiki;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = WikiApplicationTests.class)
 class WikiApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("hello");
     }
 
 }
