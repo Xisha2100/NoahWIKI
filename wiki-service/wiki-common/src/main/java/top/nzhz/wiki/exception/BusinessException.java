50e3ed85d790f05c3c453438397bcd0c0c9276e8
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-05-17 17:34:25 +0800

    171 微服务common 增加通用异常

---
+++ wiki-service/wiki-common/src/main/java/top/nzhz/wiki/exception/BusinessException.java
@@ -0,0 +1,34 @@
+package top.nzhz.wiki.exception;
+
+/**
+ * @Class: IgnoreRespAdvice
+ * @Description: 通用异常定义
+ * @Author: Xisha
+ * @Date: 2022/05/17 21:35
+ **/
+
+public class BusinessException extends RuntimeException{
+
+    private BusinessExceptionCode code;
+
+    public BusinessException (BusinessExceptionCode code) {
+        super(code.getDesc());
+        this.code = code;
+    }
+
+    public BusinessExceptionCode getCode() {
+        return code;
+    }
+
+    public void setCode(BusinessExceptionCode code) {
+        this.code = code;
+    }
+
+    /**
+     * 不写入堆栈信息，提高性能
+     */
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+}
